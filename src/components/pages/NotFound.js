6574aabcd764e5235b20b5a7ec48e3259e6246d9
Dnyn Shinde <shindekartik811@gmail.com>
2020-09-17 10:48:17 +0530

    Added Context API

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const NotFound = () => {
+  return (
+    <Fragment>
+      <div>
+        <h1>Oop's! Page Not Found</h1>
+        <p className="lead">The page you are looking for does not exist...</p>
+      </div>
+    </Fragment>
+  );
+};
+
+export default NotFound;
